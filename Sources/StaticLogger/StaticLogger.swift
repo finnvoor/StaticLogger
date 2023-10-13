517960a9546011fbf1ef4bc9dbe6e4832ec1b98c
Finn Voorhees <finnvoorhees@gmail.com>
2023-10-13 23:49:00 +0100

    Fix documentation

--- Sources/StaticLogger/StaticLogger.swift
+++ Sources/StaticLogger/StaticLogger.swift
@@ -1,7 +1,7 @@
-// Adds a static `logger` member to the type.
-// - Parameters:
-//   - subsystem: An optional subsystem for the logger to use. Defaults to `Bundle.main.bundleIdentifier`.
-//   - category: An optional category for the logger to use. Defaults to `String(describing: Self.self)`.
+/// Adds a static `logger` member to the type.
+/// - Parameters:
+///   - subsystem: An optional subsystem for the logger to use. Defaults to `Bundle.main.bundleIdentifier`.
+///   - category: An optional category for the logger to use. Defaults to `String(describing: Self.self)`.
 @attached(member, names: named(logger)) public macro StaticLogger(
     subsystem: String? = nil,
     category: String? = nil
